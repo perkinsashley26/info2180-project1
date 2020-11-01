89cf7ff52188ecf31582744af0013eb42339b01b
Ashley perkins <perkinsashley26@gmail.com>
2020-11-01 18:32:36 -0500

    Edited Document

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    let email= document.getElementById("email");
+    let message = document.getElementsByClassName("message")[0];
+    let button= document.getElementsByTagName("button")[0];
+    button.onclick=function(event){
+        event.preventDefault();
+        if (email.value===""){
+            message.innerHTML="Please enter valid email address";
+        }
+        else{
+            message.innerHTML="Thank You! Your email address has been added to our mailing list";
+        }
+    }
+}
